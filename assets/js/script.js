9a29d5c8cf1450817a5240b50cf50da82a9ff3b9
alex.kim <alkkim@gmail.com>
2022-11-16 12:18:26 -0800

    created questions and answers

---
+++ assets/js/script.js
@@ -0,0 +1,63 @@
+var qAnda = [
+    {
+        question: "What yeard was javascript created?",
+        choices: ["2000", "2001", "1997", "1995"],
+        answer: 4
+    },
+
+    {
+        question: "What will the following code return in value: 3 < 7",
+        choices: ["true", "false", "NaN", "alert"],
+        answer: 1
+    },
+
+    {
+        question: "What file extension does Javascript have",
+        choices: [".java", ".js", ".javascript", ".html"],
+        answer: 2
+    },
+
+    {
+        question: "What HTML element do we use to link a .js file",
+        choices: ["class", "link", "script", "@import"],
+        answer: 3
+    },
+
+    {
+        question: "How do you create a function in JavaScript",
+        choices: ["function = myFunction()", "function myFunction()", "function", "var function"], 
+        answer: 2
+    },
+
+    {
+        question: "What event occurs when the user clicks on an HTML element?",
+        choices: ["mouseover", "alert", "action", "onclick"],
+        answer: 4
+    },
+
+    {
+        question: "How do you declare a variable in JavaScript?",
+        choices: ["jsVar = ", "v jsVar: ", "--jsVar: (var)", "var jsVar = "],
+        answer: 4
+    },
+
+    {
+        question: "Where is the correct place to insert Javascript on a webpage?",
+        choices: ["inside the <body>", "insdide the <head>", "inside <head> & <body>", "inside <footer>"],
+        answer: 3
+    },
+
+    {
+        question: "What is the correct commenting syntax in JavaScript?",
+        choices: ["/*Comment*/", "<!--Comment-->", "#Comment", "Comment = "],
+        answer: 1
+    },
+
+    {
+        question: "What does a 'for' do?",
+        choices: ["repeats forever", "used only in functions", "repeats until a specific condition evaluates to false", "only exist in Python"],
+        answer: 3
+    }
+
+]
+
