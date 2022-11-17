a646272608cdc784c7a0795ebf12577d685e5813
alex.kim <alkkim@gmail.com>
2022-11-17 09:53:16 -0800

    update

--- assets/js/list.js
+++
@@ -1,8 +0,0 @@
-var listCount = document.querySelectorAll("li");
-var choices = [".java", ".js", ".javascript", ".html"];
-
-console.log(listCount[1]);
-
-for (let x = 0; x < listCount.length; x++){
-    document.querySelectorAll("li")[x].innerHTML = choices[x];
-}
--- assets/js/script.js
+++ assets/js/script.js
@@ -2,61 +2,61 @@ var qAnda = [
     {
         question: "What year was javascript created?",
         choices: ["2000", "2001", "1997", "1995"],
-        answer: 3
+        answer: "1995"
     },
 
     {
         question: "What will the following code return in value: 3 < 7",
         choices: ["true", "false", "NaN", "alert"],
-        answer: 0
+        answer: "true"
     },
 
     {
         question: "What file extension does Javascript have",
         choices: [".java", ".js", ".javascript", ".html"],
-        answer: 1
+        answer: ".js"
     },
 
     {
         question: "What HTML element do we use to link a .js file",
         choices: ["class", "link", "script", "@import"],
-        answer: 2
+        answer: "script"
     },
 
     {
         question: "How do you create a function in JavaScript",
         choices: ["function = myFunction()", "function myFunction()", "function", "var function"],
-        answer: 1
+        answer: "function myFunction()"
     },
 
     {
         question: "What event occurs when the user clicks on an HTML element?",
         choices: ["mouseover", "alert", "action", "onclick"],
-        answer: 3
+        answer: "onclick"
     },
 
     {
         question: "How do you declare a variable in JavaScript?",
         choices: ["jsVar = ", "v jsVar: ", "--jsVar: (var)", "var jsVar = "],
-        answer: 3
+        answer: "var jsVar = "
     },
 
     {
-        question: "Where is the correct place to insert Javascript on a webpage?",
-        choices: ["inside the body", "inside the head", "inside head & body", "inside footer"],
-        answer: 1
+        question: "Which of the following type of variable is visible everywhere in your JavaScript code?",
+        choices: ["global variable", "local variable", "Both of the above", "None of the above"],
+        answer: "global variable"
     },
 
     {
-        question: "What is the correct commenting syntax in JavaScript?",
-        choices: ["/*Comment*/", "<!--Comment-->", "#Comment", "Comment = "],
-        answer: 0
+        question: "What are variables used for in JavaScript Programs?",
+        choices: ["Storing numbers, dates, or other values", "Used in calling a function", "Varying randomly", "None of the above"],
+        answer: "Storing numbers, dates, or other values"
     },
 
     {
         question: "What does a 'for' loop do?",
         choices: ["repeats forever", "used only in functions", "repeats until a specific condition evaluates to false", "only exist in Python"],
-        answer: 2
+        answer: "repeats until a specific condition evaluates to false"
     }
 
 ]
@@ -70,35 +70,43 @@ var count = 0;
 
 //get questions from the the qAnda array
 function getQuestions(quiz) {
+    console.log(quiz);
     questionEl.textContent = quiz.question;
-    var correctAnswer = quiz.choices[quiz.answer];
-    console.log(correctAnswer)
+    console.log(quiz.answer);
 
     for (let x = 0; x < listEl.length; x++) {
         listEl[x].innerHTML = quiz.choices[x]
     };
 
-    answerEl.addEventListener("click", function (event) {
-        var element = event.target;
-        
-        if (element.matches(".box")) {
-            var chosenAnswer = element.textContent;
-            if (chosenAnswer === correctAnswer){
-                console.log('correct');
-                count++;
-            } else {
-                console.log("wrong");
-                count++;
-            }
-        }
-        
-        getQuestions(qAnda[count]);
-    })
+    getAnswers(quiz);
 }
 
+function getAnswers(quiz) {
+    answerEl.addEventListener("click", function (event) {
+        var element = event.target;
+        var correctAnswer = quiz.answer;
+        console.log(correctAnswer);
 
-function getAnswers() {
+        if (element.matches(".box")) {
+            var chosenAnswer = element.textContent;
+            console.log(chosenAnswer);
+            console.log(chosenAnswer == correctAnswer);
+            count++;
+            if (chosenAnswer == correctAnswer) {
+                count++;
+                console.log('correct');
+                correctAnswer = "";
+                getQuestions(qAnda[count]);
+            } else if (chosenAnswer != correctAnswer) {
+                count++;
+                console.log("wrong");
+                correctAnswer = "";
+                getQuestions(qAnda[count]);
+            }
+        }
 
+
+    })
 }
 
 //start game button unhides questions and answers portion and hides the start the quiz game button
@@ -113,6 +121,5 @@ startEl.addEventListener("click", function (event) {
             mainGameEl.style.display = "block";
         }
     }
-    getQuestions(qAnda[0]);
+    getQuestions(qAnda[count]);
 })
-
